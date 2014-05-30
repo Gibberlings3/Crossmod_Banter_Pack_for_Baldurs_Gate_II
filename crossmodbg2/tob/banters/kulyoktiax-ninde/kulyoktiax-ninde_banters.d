fba063883960388c4eb66743e8ea1ad06661acd9
cmorganbg <cmorganbg@gmail.com>
2014-05-30 14:23:29 -0500

    Adding SoA and ToB Ninde-Coran|Tiax|Xan
    kulyok<> Rhaella and Kaeloree authorized
    Post No. 104
    http://forums.gibberlings3.net/index.php?showtopic=23641&p=207767

---
+++ crossmodbg2/tob/banters/kulyoktiax-ninde/kulyoktiax-ninde_banters.d
@@ -0,0 +1,16 @@
+// 1.
+
+CHAIN 
+IF ~InParty("Ninde")
+See("Ninde")
+!StateCheck("Ninde",CD_STATE_NOTVALID)
+!StateCheck("O#Tiax",CD_STATE_NOTVALID)
+CombatCounter(0)
+!See([ENEMY])
+Global("G#XB.TiaxNindeToBBanter1","GLOBAL",0)~ THEN BO#TIA25 NindeTiaxToBBanter#1.1
+~See? What did the Great Tiax tell you? He will RULE ALL! And now he does! A Pocket Plane, a holy symbol of the most unholy Cyric, and planetars at his beck and call!~
+DO ~SetGlobal("G#XB.TiaxNindeToBBanter1","GLOBAL",1)~
+== BLK#NI25 ~Really, dear? What if a powerful necromancer with sultry looks and saucy manners kills you? Then she can bring you back to life. Won't she have your power at her beck and call, then?~
+== BO#TIA25 ~Er... Tiax did not think of this? Umm...~
+== BLK#NI25 ~And she plays a mean game of cards, too.~
+EXIT
